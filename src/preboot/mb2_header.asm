cd7072c68817d17226a0b2c31d42cea156801931
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-04-19 10:29:47 -0400

    Add multiboot2 support

---
+++ src/preboot/mb2_header.asm
@@ -0,0 +1,16 @@
+.section mb2_header
+header_start:
+    ; https://www.gnu.org/software/grub/manual/multiboot2/multiboot.html for more information, section 3.1.1
+    dd 0xe85250d6 ; multiboot2 magic number
+    
+    dd 0 ; i386 protected 32 bit mode
+    
+    dd header_end - header_start ; Header length
+    
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start)) ; Checksum, total is 0
+    
+    ; Ending tag
+	  dw 0
+	  dw 0
+	  dd 8
+header_end:
