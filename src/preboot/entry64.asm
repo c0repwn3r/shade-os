0c593d9574f7c4be152f924f4ab8df8f13550864
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-04-19 09:36:35 -0400

    Update entry64.asm

--- src/preboot/entry64.asm
+++ src/preboot/entry64.asm
@@ -3,4 +3,15 @@ extern kernel_entry
 
 section .text
 bits 64
-entry_x64
+entry_x64:
+    ; Nullify all data registers
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    
+    call kernel_entry ; Jump into C kernel entry
+    
+    hlt
