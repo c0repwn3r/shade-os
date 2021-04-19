4099b91dc67569cdb645baaa020210239de77676
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-04-19 11:22:09 -0400

    Add kernel debug stuff

--- src/kernel/kernel_entry.c
+++ src/kernel/kernel_entry.c
@@ -1,5 +1,10 @@
 #pragma once
 
+#include "print.h"
+
 void kernel_entry() {
-  
+  // 64 bit kernel entry - preboot is fully set up
+  print_clear(); // Clear screen from bootloader stuff
+  print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK); // Set format to classic white on black
+  print_str("shadeOS in 64 bit kernel - debug build"); // print a little debug message
 }
