6aa54fb158fccd7501996ae7f238355ca729f9e1
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-04-20 11:24:16 -0400

    Update kern_logging.h

--- src/interface/kern_logging.h
+++ src/interface/kern_logging.h
@@ -11,4 +11,8 @@ void kernel_warn(char* string); // [ WARN ] Message
 void kernel_error(char* string); // [ FAILED ] Message
 void kernel_fatal(char* string); // [ FATAL ] Message
 
-void process_debug(char* thread, char* message); // [ DEBUG/Thread ] Message
+void thread_debug(char* thread, char* message); // [ DEBUG/Thread ] Message
+void thread_ok(char* thread, char* message); // [ Thread ] Message
+void thread_warn(char* thread, char* message); // [ WARN/Thread ] Message
+void thread_error(char* thread, char* message); // [ ERROR/Thread ] Message
+void thread_fatal(char* thread, char* message); // [ FATAL/Thread ] Message
