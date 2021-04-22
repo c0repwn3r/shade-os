303d1260ab62b1a48f0ba19357498d441f11049d
GhostlyCoding <49044974+GhostlyCoding@users.noreply.github.com>
2021-04-22 18:34:52 -0500

    Create kernel_module_welcome.h

---
+++ src/interface/kernel_module_welcome.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "print.h"
+
+void kernel_module_welcome_load();
+void kernel_module_welcome_invoke();
+void kernel_module_welcome_unload();
